1d184053bbe3ac5e20c4e12e32d532a6e6678974
dkrano <dkrano@13a3c72f-2b92-48d8-bf59-54b44103bb54>
2015-05-10 01:22:25 +0000

    PT-2021: minimize the temporary integration test.
    git-svn-id: http://ubersvn.nikedev.com:9880/bluegreen-manager/trunk@112 13a3c72f-2b92-48d8-bf59-54b44103bb54

--- src/main/java/com/nike/tools/bgm/jobs/TeardownJob.java
+++ src/main/java/com/nike/tools/bgm/jobs/TeardownJob.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 
 import com.nike.tools.bgm.model.domain.JobHistory;
 import com.nike.tools.bgm.tasks.LocalShellConfig;
-import com.nike.tools.bgm.tasks.LocalShellTask;
 import com.nike.tools.bgm.tasks.SshVmDeleteTask;
 import com.nike.tools.bgm.tasks.Task;
 
@@ -68,7 +67,7 @@ public class TeardownJob extends TaskSequenceJob
     defineSubstitutionsForShutdownApplications();
     int position = 1;
     List<Task> tasks = new ArrayList<Task>();
-    tasks.add(applicationContext.getBean(LocalShellTask.class).assign(position++, deleteEnvName, shutdownApplicationsConfig));
+    //tasks.add(applicationContext.getBean(LocalShellTask.class).assign(position++, deleteEnvName, shutdownApplicationsConfig));
     tasks.add(applicationContext.getBean(SshVmDeleteTask.class).init(position++, deleteEnvName));
     //tasks.add(applicationContext.getBean(LocalShellTask.class).assign(position++, deleteEnvName, deleteEnvConfig));
     //tasks.add(applicationContext.getBean(RdsInstanceDeleteTask.class).init(position++, deleteEnvName, deleteDbPhysicalInstanceName));
