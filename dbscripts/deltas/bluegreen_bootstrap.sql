10212a0e00622fbb81ba233bdaaa27097dd1d326
dkrano <dkrano@13a3c72f-2b92-48d8-bf59-54b44103bb54>
2015-05-24 05:43:19 +0000

    Add sql example to bootstrap an env.
    git-svn-id: http://ubersvn.nikedev.com:9880/bluegreen-manager/trunk@159 13a3c72f-2b92-48d8-bf59-54b44103bb54

---
+++ dbscripts/deltas/bluegreen_bootstrap.sql
@@ -0,0 +1,19 @@
+-- This is just an example of how you would bootstrap the blue/green data model
+-- to describe your first environment.  You would need to change all these values
+-- to make them specific for you.
+
+insert into ENVIRONMENT (ENV_ID, ENV_NAME) values (1, 'env1');
+
+insert into APPLICATION_VM (APPVM_ID, APPVM_HOSTNAME, APPVM_IP_ADDRESS, FK_ENV_ID) values
+(1, 'vm0100.example.com', '10.111.222.100', 1);
+
+insert into APPLICATION (APP_ID, APP_SCHEME, APP_HOSTNAME, APP_PORT, APP_URL_PATH, FK_APPVM_ID) values
+(1, 'http', 'vm0100.example.com', 8080, '/rest/bluegreen', 1);
+
+insert into LOGICAL_DATABASE (LOGICAL_ID, LOGICAL_NAME, FK_ENV_ID) values
+(1, 'the_datasource', 1);
+
+insert into PHYSICAL_DATABASE (PHYSICAL_ID, PHYSICAL_TYPE, DRIVER_CLASS_NAME, PHYSICAL_INST_NAME,
+                               IS_LIVE, `PASSWORD`, URL, USERNAME, FK_LOGICAL_ID) values
+(1, 'RDS', 'com.mysql.jdbc.Driver', 'my-rds-instance', 1, 'abc123',
+ 'jdbc:mysql://my-rds-instance.wwcckkadf.us-west-2.rds.amazonaws.com:3306/dbname', 'admin', 1);
